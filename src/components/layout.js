b76fbf1e734862ebecf9566086094e8a7012baa3
connor <connor.jaksik@gmail.com>
2019-10-27 19:51:13 -0600

    updated nav bar

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import pic from "../images/gatsby-icon.png"
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+
 import "./header.css"
 import "./layout.css"
 
@@ -9,55 +11,46 @@ function Layout ({ children }) {
   const [open, toggle] = useState(false);
 
   return (
-    <div>
+    <div className="wrapper">
+
       <div className={`nav-bar ` + (open ? `nav-open` : ``)}>
-          <div className="nav-items">
-            <Link className="nav-item">
-              Home
-            </Link><br/>
-            <Link to="/gallery&prints" className="nav-item">
-              Gallery & Prints
-            </Link><br/>
-            <Link className="nav-item">
-              Services
-            </Link><br/>
-            <Link className="nav-item">
-              Contact
-            </Link>
-          </div>
+        <div className="nav-items">
+          <AniLink fade to="/" className="nav-item">
+            <p className="nav-item">Home</p>
+          </AniLink>
+          <AniLink fade to="/gallery&prints" className="nav-item">
+            <p className="nav-item">Gallery & Prints</p>
+          </AniLink>
+          <AniLink fade to="/contact" className="nav-item">
+            <p className="nav-item">Contact</p>
+          </AniLink>
+        </div>
 
-          <div className="nav-content">
-            <span onClick={() => toggle(!open)}>
-              <div className={`mobile-icon ` + (open ? `change` : ``)}>
-                <div class="bar1"></div>
-                <div class="bar2"></div>
-                <div class="bar3"></div>
-              </div>
-            </span>
+        <div className="nav-content">
+          <span onClick={() => toggle(!open)} style={{position:`absolute`}}>
+            <div className={`mobile-icon ` + (open ? `change` : ``)}>
+                  <div class="bar1"></div>
+                  <div class="bar2"></div>
+                  <div class="bar3"></div>
+            </div>
+          </span>
 
-            <Link to="/" style={{ textDecoration: `none`, width:`100%`}} className="primary-color nav-brand">
-              <img src={pic} style={{width:`50px`, height: `50px`, margin: `20px auto`}}/>
-              <br/>
-              <h1>Aspen Media</h1>
-            </Link>
+          <Link to="/" style={{ textDecoration: `none`, width:`100%`}} className="primary-color nav-brand">
+            <h1>Aspen Media</h1>
+          </Link>
 
-            <div className="desktop-nav-items">
-              <Link className="nav-item">
-                Home
-              </Link><br/>
-              <Link to="/gallery&prints" className="nav-item">
-                Gallery & Prints
-              </Link><br/>
-              <Link className="nav-item">
-                Services
-              </Link><br/>
-              <Link className="nav-item">
-                Contact
-              </Link>
-          </div>
-            
+          <div className="desktop-nav-items">
+            <AniLink fade to="/">
+              <p className="nav-item">Home</p>
+            </AniLink>
+            <AniLink fade to="/gallery&prints" className="nav-item">
+              <p>Gallery & Prints</p>
+            </AniLink>
+            <AniLink fade to="/contact" className="nav-item">
+              <p>Contact</p>
+            </AniLink>
           </div> 
-
+        </div> 
       </div>
 
       <main className={`main ` + (open ? `open` : ``)}>{children}</main>
--- src/pages/gallery&prints.js
+++ src/pages/gallery&prints.js
@@ -7,26 +7,27 @@ import "./gallery.css"
 import "./index.css"
 
 const GalleryPage = ({ data }) => { 
-    console.log("data: ", data)
-    return (
-        <div style={{position:`fixed`, width:`100vw`, height:`100vh`, overflow:`hidden`}}>
-            <Layout>
-            <SEO title="Home" />
-            <h1>Gallery and Prints</h1>
-            {data.allMarkdownRemark.edges.map((edge, index) => {
-                const collection = edge.node.frontmatter
-                return (
-                <Link to={edge.node.fields.slug}>
-                    <div className="card-wrapper">
-                        <Img fluid={collection.image.childImageSharp.fluid} style={{ position:`absolute`, width:`100%`}}/>
-                        <h3 className="card-title">{collection.title}</h3>
-                    </div>
-                </Link>
-            )})}
-            
-            </Layout>
-        </div>
-)}
+  console.log("data: ", data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div style={{backgroundColor:`rgba(24, 21, 16, 1)`, height:`100%`}}>
+      <h1 style={{textAlign:`center`}} className="primary-color">Gallery and Prints</h1>
+      {data.allMarkdownRemark.edges.map((edge, index) => {
+        const collection = edge.node.frontmatter
+        return (
+          <Link to={edge.node.fields.slug}>
+              <div className="card-wrapper">
+                  <Img fluid={collection.image.childImageSharp.fluid} style={{ position:`absolute`, width:`100%`}}/>
+                  <h3 className="card-title">{collection.title}</h3>
+              </div>
+          </Link>
+        )
+      })}
+      </div>
+    </Layout>
+  )
+}
 
 export default GalleryPage
 
