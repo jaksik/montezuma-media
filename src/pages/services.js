62ce78b2535659aac3f4fd61cb785b682dee9324
connor <connor.jaksik@gmail.com>
2019-11-10 23:41:57 -0700

    obile

--- src/pages/services.js
+++ src/pages/services.js
@@ -11,7 +11,7 @@ const ServicesPage = ({ data }) => (
     <Layout>
       <h1 className="primary-color text-center">Services</h1>
       <Row className="no-gutters align-items-center">
-        <Col xs="12" sm="8" lg="7"className="primary-color pr-sm-2">
+        <Col xs="12" sm="6" md="7" lg="8"className="primary-color pr-sm-2">
           <h2>Adventure Follows</h2>
           <h4>$149/hour or $499/day</h4>
           <p>Do you go on awesome adventures and need a photographer? Connor offers daily and hourly rates to acompany you as a photographer on your next adventure.</p>
@@ -21,18 +21,15 @@ const ServicesPage = ({ data }) => (
             <li>Ski Follows</li>
           </ul>
         </Col>
-        <Col xs="12" sm="4" lg="5" className="align-items-center" style={{width:`40%`}}>
+        <Col xs="12" sm="6" md="5" lg="4" className="align-items-center" style={{width:`40%`}}>
           <Img fluid={data.file.childImageSharp.fluid} style={{}}/>
         </Col>
       </Row>
 
       <div className="line-divider"></div>
 
-      <Row className="row">
-        <div className="col" style={{width:`40%`}}>
-          <Img fluid={data.file.childImageSharp.fluid} style={{}}/>
-        </div>
-        <div className="primary-color col" style={{width:`60%`}}>
+      <Row className="no-gutters align-items-center">
+        <Col xs="12" sm="6" md="7" lg="8"className="primary-color pr-sm-2">
           <h2>Adventure Follows</h2>
           <h4>$149/hour or $499/day</h4>
           <p>Do you go on awesome adventures and need a photographer? Connor offers daily and hourly rates to acompany you as a photographer on your next adventure.</p>
@@ -41,23 +38,15 @@ const ServicesPage = ({ data }) => (
             <li>Overnight Trips</li>
             <li>Ski Follows</li>
           </ul>
-        </div>
+        </Col>
+        <Col xs="12" sm="6" md="5" lg="4" className="align-items-center" style={{width:`40%`}}>
+          <Img fluid={data.file.childImageSharp.fluid} style={{}}/>
+        </Col>
       </Row>
 
       <div className="line-divider"></div>
       
-      <div className="row no-gutters" style={{flexDirection:`column`}}>
-      <Col xs="12">
-      <h2 className="primary-color text-center">Contact Us</h2>
-
-      <form style={{margin:`0 auto`}}>
-        <input placeholder=" Name*"/>
-        <input placeholder=" Email*"/><br/>
-        <input placeholder=" Phone"/>
-        <input placeholder="Email"/>
-      </form>
-      </Col>
-      </div>
+      <button>Contact Us</button>
     </Layout>
   </div>
 )
