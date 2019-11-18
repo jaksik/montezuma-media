5773851258b420c2c163dd2e11af6efee5938246
connor <connor.jaksik@gmail.com>
2019-11-17 20:08:46 -0700

    added form functionality

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,17 @@ const ContactPage = ({ data }) => {
       <SEO title="Home" />
       <Img fluid={data.backgroundImage.childImageSharp.fluid} style={{maxHeight:`300px`}}/>
       <h2 className="primary-color text-center mt-5">Send Us a Message</h2>
-      <form style={{margin:`20 auto`}}>
+
+      <form 
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        style={{margin:`20 auto`}}
+      >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+        <input style={{display: `none`}} type="text" />
         <strong className="mb-3">Tell us about your next adventure, or how you would like to bring inspiration to your space.</strong>
         <input className="mb-3 mt-3" placeholder=" Name*"/>
         <input className="mb-3" placeholder=" Email*"/>
